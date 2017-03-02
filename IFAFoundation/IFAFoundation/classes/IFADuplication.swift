3fa82248c79b099109dd292cf7ef34a353faa5d5
Marcelo Schroeder <marcelo.schroeder@infoaccent.com>
2017-03-03 07:17:40 +1100

    [NEW] Added IFADuplication.swift.

---
+++ IFAFoundation/IFAFoundation/classes/IFADuplication.swift
@@ -0,0 +1,10 @@
+//
+// Created by Marcelo Schroeder on 3/3/17.
+// Copyright (c) 2017 InfoAccent Pty Ltd. All rights reserved.
+//
+
+import Foundation
+
+@objc public protocol IFADuplication {
+    @objc func name() -> String
+}
