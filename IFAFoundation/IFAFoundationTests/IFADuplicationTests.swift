3572fd50f83e33ded4b63cd318788985eac74603
Marcelo Schroeder <marcelo.schroeder@infoaccent.com>
2017-03-04 13:55:50 +1100

    [NEW] Added IFADuplication.swift.

--- IFAFoundation/IFAFoundationTests/IFADuplicationTests.swift
+++ IFAFoundation/IFAFoundationTests/IFADuplicationTests.swift
@@ -8,22 +8,9 @@ import XCTest
 @testable import IFAFoundation
 
 class IFADuplicationTests: XCTestCase {
-
-    func testName() {
-
-        // Given
-        let duplicateSource = IFADuplicationTestClass("Test")
-        let existingItems: Array<IFADuplication> = [duplicateSource]
-
-        // When
-        let result = IFADuplicationUtils.name(forDuplicateOf: duplicateSource, existingItems: existingItems)
-
-        // Then
-        XCTAssertEqual(result, "Test Copy", "")
-
-    }
     
     func testSignificantDuplicationRegexGroup() {
+
         XCTAssertEqual(IFADuplicationUtils.significantDuplicationRegexGroup(forName: "Report"), nil)
         XCTAssertEqual(IFADuplicationUtils.significantDuplicationRegexGroup(forName: "Report Copy"), "Copy")
         XCTAssertEqual(IFADuplicationUtils.significantDuplicationRegexGroup(forName: "Report Copy 2"), "2")
@@ -32,6 +19,7 @@ class IFADuplicationTests: XCTestCase {
         XCTAssertEqual(IFADuplicationUtils.significantDuplicationRegexGroup(forName: "Copy 23 Report"), nil)
         XCTAssertEqual(IFADuplicationUtils.significantDuplicationRegexGroup(forName: "Report Copy 12 Copy 23"), "23")
         XCTAssertEqual(IFADuplicationUtils.significantDuplicationRegexGroup(forName: nil), nil)
+    
     }
     
     func testHighestCopySequence() {
@@ -64,6 +52,32 @@ class IFADuplicationTests: XCTestCase {
             IFADuplicationTestClass("Test Copy 23"),
             ]), 23)
 
+    }
+    
+    func testName() {
+        
+        XCTAssertEqual(IFADuplicationUtils.name(forDuplicateOf: IFADuplicationTestClass("Test"), existingItems: [
+            IFADuplicationTestClass("Test"),
+            ]), "Test Copy")
+        
+        XCTAssertEqual(IFADuplicationUtils.name(forDuplicateOf: IFADuplicationTestClass("Test"), existingItems: [
+            IFADuplicationTestClass("Test"),
+            IFADuplicationTestClass("Test Copy"),
+            ]), "Test Copy 2")
+        
+        XCTAssertEqual(IFADuplicationUtils.name(forDuplicateOf: IFADuplicationTestClass("Test"), existingItems: [
+            IFADuplicationTestClass("Test"),
+            IFADuplicationTestClass("Test Copy"),
+            IFADuplicationTestClass("Test Copy 2"),
+            ]), "Test Copy 3")
+        
+        XCTAssertEqual(IFADuplicationUtils.name(forDuplicateOf: IFADuplicationTestClass("Test"), existingItems: [
+            IFADuplicationTestClass("Test"),
+            IFADuplicationTestClass("Test Copy"),
+            IFADuplicationTestClass("Test Copy 2"),
+            IFADuplicationTestClass("Test Copy 23"),
+            ]), "Test Copy 24")
+        
     }
 
 }
